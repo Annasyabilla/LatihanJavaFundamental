44afb72dad1e1c3041e53be21c879e288e516757
Annasyabilla <syabilla90@gmail.com>
2024-10-01 02:56:14 +0700

    nyoba

---
+++ src/main/java/com/juaracoding/Main.java
@@ -0,0 +1,22 @@
+package com.juaracoding;
+
+//Buat class bernama "Main", nama File dan class harus sama
+public class Main {
+    //standarisasi nama class DepannyaKapital
+    public static void main(String[] args) {
+        int x = 100;
+
+        System.out.println("Hello world!");
+
+        if (true){
+            System.out.println("true!");
+            System.out.println(x);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println(x);
+
+        }
+    }
+}
